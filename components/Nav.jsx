8703429d29457777b5bfca4cd36d6f6218bc1a96
variyapurv <variyapurv4211@gmail.com>
2024-08-02 15:00:33 +0530

    Multiple Pages

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom'
 
 const Nav = () => {
   return (
-    <div>
-        <Link to="/" > Home </Link>
-        <Link to="/Product" > Product </Link>
-        <Link to="/Login" > Login </Link>
-        <Link to="/SignUp" > SignUp </Link>
+    <div className='nav'>
+        <Link className='sidebar' to="/" > Home </Link>
+        <Link className='sidebar' to="/Product" > Product </Link>
+        <Link className='sidebar' to="/Login" > Login </Link>
+        <Link className='sidebar' to="/SignUp" > SignUp </Link>
     </div>
   )
 }
